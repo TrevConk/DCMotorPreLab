4d07b0b32cef17adf3e978ae5506dc9a73652667
TConk <91498857+TrevConk@users.noreply.github.com>
2021-10-20 20:18:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+inputPin = 26
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(inputPin, GPIO.OUT)
+pwmMotor = GPIO.PWM(inputPin, 100)
+pwmMotor.start(0)
+
+try:
+  while(True):
+    pwmMotor.ChangeDutyCycle(int(input('Enter the speed of the motor:')))
+except Exception as e:
+  print('There was an error',e)
+
+
+
+
+
